0a237d05d23e924c3b29747214922f7728a2e782
quiveringbacon <128983862+quiveringbacon@users.noreply.github.com>
2025-09-22 11:29:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -199,7 +199,7 @@ resource "azurerm_virtual_network" "hub-vnet" {
   name                = "AZ-hub-vnet"
   resource_group_name = azurerm_resource_group.RG.name
   subnet {
-    address_prefix     = ["10.0.0.0/24"]
+    address_prefixes     = ["10.0.0.0/24"]
     name                 = "default"
     security_group = azurerm_network_security_group.hubvnetNSG.id
   }
@@ -960,3 +960,4 @@ router bgp 65001
 
 CUSTOM_DATA  
 }
+
